e5dbb70a06ad477724558c833763115b4dba69f2
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2022-10-18 19:05:09 -0500

    use the current directory

--- lib/unifig/providers/env-file.rb
+++ lib/unifig/providers/env-file.rb
@@ -4,13 +4,16 @@ module Unifig
   module Providers
     # @private
     module EnvFile
+      PATH = File.join(Dir.pwd, '.env')
+      private_constant :PATH
+
       class << self
         def name
           :'env-file'
         end
 
         def retrieve(var_names, config)
-          env_vars = Unifig::Env::Parser.call(config.fetch(:file, '.env'))
+          env_vars = Unifig::Env::Parser.call(config.fetch(:file, PATH))
 
           var_names.to_h do |name|
             [name, env_vars[name.to_s]]
